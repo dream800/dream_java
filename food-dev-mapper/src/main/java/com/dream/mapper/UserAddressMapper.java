2b3a79b094f712197845a573f0ff75f977c133e0
dream <1831761821@qq.com>
2022-06-05 15:14:09 +0800

    开发收货地址

--- food-dev-mapper/src/main/java/com/dream/mapper/UserAddressMapper.java
+++ food-dev-mapper/src/main/java/com/dream/mapper/UserAddressMapper.java
@@ -3,5 +3,16 @@ package com.dream.mapper;
 import com.dream.my.mapper.MyMapper;
 import com.dream.pojo.UserAddress;
 
+import java.util.List;
+
 public interface UserAddressMapper extends MyMapper<UserAddress> {
+
+
+    public void insertAddress(UserAddress userAddress);
+
+
+    public List<UserAddress> selectAddressByUid(String userId);
+
+
+
 }
